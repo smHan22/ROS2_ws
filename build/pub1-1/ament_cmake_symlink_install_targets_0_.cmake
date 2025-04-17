afcad576889bccdf25b5562c83899a02217f8fc9
SMHAN22 <90818408+smHan22@users.noreply.github.com>
2025-04-17 16:34:39 +0900

    Delete build directory

--- build/pub1-1/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/seongmin/ros2_ws/build/pub1-1/pub1-1" "TARGETS" "pub1-1" "DESTINATION" "lib/pub1-1")
--- build/pub1-3/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/seongmin/ros2_ws/build/pub1-3/pub1-3" "TARGETS" "pub1-3" "DESTINATION" "lib/pub1-3")

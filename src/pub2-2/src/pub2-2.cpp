a43b48f2e57d27ce114ae43ee652b66455cd3120
SMHAN22 <90818408+smHan22@users.noreply.github.com>
2025-04-21 17:27:29 +0900

    Update pub2-2.cpp

--- src/pub2-2/src/pub2-2.cpp
+++ src/pub2-2/src/pub2-2.cpp
@@ -29,13 +29,10 @@ int main(int argc, char* argv[])
     auto node = std::make_shared<rclcpp::Node>("mynode");
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
 
-    // Vector3 메시지 타입 퍼블리셔 생성
     auto pub = node->create_publisher<geometry_msgs::msg::Vector3>("mytopic", qos_profile);
 
-    // 콜백 함수 바인딩
     std::function<void()> fn = std::bind(callback, node, pub);
 
-    // 타이머 콜백 설정 (1초 간격, 하지만 cin으로 블로킹됨)
     auto timer = node->create_wall_timer(1s, fn);
 
     rclcpp::spin(node);
